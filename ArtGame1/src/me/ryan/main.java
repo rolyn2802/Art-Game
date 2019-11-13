a71c551df41f25b6343d5c9bab21725a3b7fe913
ryan.prince <ryan.prince@PC071-L118-03.boone.ketsds.net>
2019-11-13 13:44:46 -0500

    change

--- ArtGame1/src/me/ryan/main.java
+++ ArtGame1/src/me/ryan/main.java
@@ -1,9 +1,5 @@
 package me.ryan;
 
-import javax.swing.JFrame;
-
-//Import the JFrame
-
 public class main {                                                                         
       public static void main(String[] args) {                                                 
               
